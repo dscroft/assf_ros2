5f9675822b06a6f23175949099880c894dd8173c
dscroft <dscroft@gmail.com>
2024-12-12 10:11:36 +0000

    Update ros_bashrc.sh
    Changed to humble

--- docker/ros_bashrc.sh
+++ docker/ros_bashrc.sh
@@ -4,8 +4,8 @@
 export PYTHONWARNINGS="ignore:easy_install command is deprecated"
 
 # To be appended to /root/.bashrc
-source /opt/ros/jazzy/setup.bash
+source /opt/ros/humble/setup.bash
 source /usr/share/colcon_cd/function/colcon_cd.sh
 source /root/dev/turtlesim/turtlesim_ws/install/setup.bash
 
-export _colcon_cd_root=/opt/ros/jazzy
+export _colcon_cd_root=/opt/ros/humble

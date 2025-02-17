0df30815e3bf807a31f6dc8bfb874d3954373581
David <david@somewebsite.co.uk>
2025-02-17 16:34:01 +0000

    Add py cloud filter structure

---
+++ cloud_ws/src/py_cloud_filter/py_cloud_filter/filter.py
@@ -0,0 +1,81 @@
+import pcl
+import random
+import rclpy
+from rclpy.node import Node
+import sensor_msgs_py.point_cloud2
+from sensor_msgs.msg import PointCloud2
+import numpy as np
+
+class Filter(Node):
+    def __init__(self):
+        """
+        Create a ROS Node that extracts a table (or other flat surface) from a point cloud.
+
+        Subscriptions
+        -------------
+        pcl_handler (sensor_msgs/msg/PointCloud2): The point cloud to publish
+
+        Publishers
+        ----------
+        pcl_cropped (sensor_msgs/msg/PointCloud2): The cropped point cloud
+        pcl_voxel   (sensor_msgs/msg/PointCloud2): The voxelized point cloud
+        pcl_inplane (sensor_msgs/msg/PointCloud2): The plane extracted from the point cloud
+        """
+        super().__init__("py_filter")
+        self.__sub = self.create_subscription(PointCloud2, "~/input", self.__callback, 10)
+        self.__pub = self.create_publisher(PointCloud2, "~/output", 10)
+
+    def __callback(self, msg: PointCloud2):
+        """Get the point cloud, perform some transformations, and publish them."""
+
+        # === GET INPUT ===
+        self.get_logger().info("Received point cloud message")
+
+        # convert ros2 message into numpy array        
+        numpy_cloud = sensor_msgs_py.point_cloud2.read_points_numpy(msg, ["x", "y", "z"])
+        # if you want other formats:
+        # - for a pure python list 
+        #         numpy_cloud.tolist()
+        # - for pcl
+        #         pcl.PointCloud(numpy_cloud)
+
+
+
+        # === PROCESSING ===
+        self.get_logger().info(f"Processing {len(numpy_cloud)} points")
+
+        # replace this code so that it filters out the ego vehicle from the input cloud
+        #   this code currently generates a random point cloud
+        #   it is possible to do this in pure python but using numpy or pcl will give 
+        #   better performance
+        # at the moment that code generates a series of random points so that you can see
+        #   how the data is created and stored
+        out_cloud = []
+        random_point = lambda: [ random.uniform(-3, 3) for _ in range(3) ]
+        for i in range(1000):
+            out_cloud.append(random_point())
+
+ 
+
+        # === PUBLISH OUTPUT ===
+        self.get_logger().info(f"Publishing {len(out_cloud)} points")
+
+        # convert the output point cloud to a ros message
+        out_msg = sensor_msgs_py.point_cloud2.create_cloud_xyz32(
+            msg.header,
+            out_cloud
+        )
+
+        # publish the output point cloud
+        self.__pub.publish(out_msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Filter()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
